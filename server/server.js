5f7b5bd8ab5210205f72ca20e822b466b2c71de7
Naman <contact.nmn@gmail.com>
2018-12-03 00:12:39 +0530

    Greet new user and alalert others

--- server/server.js
+++ server/server.js
@@ -20,13 +20,32 @@ io.on('connection', (socket) => {
   //   createdAt: 123123
   // });
 
+  // socket.emit from Admin text Welcome to the chat app
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app'
+  });
+
+  // socket.broadcast.emit from Admin text New user joined
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
     io.emit('newMessage', {  // emit to every single connection
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
-    })
+    });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
